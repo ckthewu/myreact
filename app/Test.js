a3543b0468a7ac69b35c40445f86b8a0c662a0ea
Chenkai Wu <wuchk@rd.netease.com>
2017-04-23 20:57:38 +0800

    first commit

---
+++ app/Test.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class text extends Component{
+    render() {
+        return (
+            <div>
+                文本内容
+            </div>
+        );
+    }
+}
+
+export default text
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    devtool: 'eval-source-map',//配置生成source map
+    entry:  __dirname + "/app/main.js",//入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devServer: {//webpack-dev-server的配置
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        colors: true,//终端中输出结果为彩色
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    },
+    module: {//在配置文件里添加loader
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+                query: {
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    },
+}
